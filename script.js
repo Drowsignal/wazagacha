24ccc772bdafc3ff95233a44cf6a7e4c2967e8ae
Drowsignal <63041152+Drowsignal@users.noreply.github.com>
2020-06-05 17:53:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $('.gachaBtn').click(function(){
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
       }
-    var ransu = getRandomIntInclusive(1,3);
+    var ransu = getRandomIntInclusive(1,26);
 
     switch(ransu){
         case 1:
@@ -23,6 +23,108 @@ $('.gachaBtn').click(function(){
             $('#res3').fadeIn();
             $('#twt03').fadeIn();
             break;
+        case 4:
+                $('#res4').fadeIn();
+                $('#twt04').fadeIn();
+                break;
+        case 5:
+            $('#res5').fadeIn();
+             $('#twt05').fadeIn();
+              break;
+        case 6:
+                $('#res6').fadeIn();
+                $('#twt06').fadeIn();
+                break;
+        case 7:
+                    $('#res7').fadeIn();
+                    $('#twt07').fadeIn();
+                    break;
+
+        case 8:
+                    $('#res8').fadeIn();
+                    $('#twt08').fadeIn();
+                    break;
+
+        case 9:
+            $('#res9').fadeIn();
+            $('#twt09').fadeIn();
+            break;
+
+        case 10:
+                $('#res10').fadeIn();
+                $('#twt10').fadeIn();
+                break;
+        case 11:
+                    $('#res11').fadeIn();
+                    $('#twt11').fadeIn();
+                    break;
+
+        case 12:
+                    $('#res12').fadeIn();
+                    $('#twt12').fadeIn();
+                    break;
+        case 13:
+                        $('#res13').fadeIn();
+                        $('#twt13').fadeIn();
+                        break;
+        case 14:
+                            $('#res14').fadeIn();
+                            $('#twt14').fadeIn();
+                            break;
+        case 15:
+             $('#res15').fadeIn();
+            $('#twt15').fadeIn();
+             break;
+        case 16:
+                $('#res16').fadeIn();
+                $('#twt16').fadeIn();
+                break;
+        case 17:
+                    $('#res17').fadeIn();
+                    $('#twt17').fadeIn();
+                    break;
+        case 18:
+                        $('#res18').fadeIn();
+                        $('#twt18').fadeIn();
+                        break;
+        case 19:
+                            $('#res18').fadeIn();
+                            $('#twt18').fadeIn();
+                            break;
+        case 20:
+                                $('#res20').fadeIn();
+                                $('#twt20').fadeIn();
+                                break;
+        case 19:
+            $('#res19').fadeIn();
+            $('#twt19').fadeIn();
+            break;
+        case 20:
+                $('#res20').fadeIn();
+                $('#twt20').fadeIn();
+                break;
+        case 21:
+                $('#res21').fadeIn();
+                $('#twt21').fadeIn();
+                break;
+        case 22:
+                $('#res22').fadeIn();
+                $('#twt23').fadeIn();
+                break;
+        case 24:
+                $('#res24').fadeIn();
+                $('#twt24').fadeIn();
+                break;
+        case 25:
+                $('#res25').fadeIn();
+                $('#twt25').fadeIn();
+                break;
+        case 26:
+                $('#res26').fadeIn();
+                $('#twt26').fadeIn();
+                break;
+                
+
 
     }
 })
